89cb10ba0d9bf7da39d8b70d43c6978cfaef58a9
AmanTiwari <er.amantiwari.2002@gmail.com>
2024-05-22 10:21:52 +0530

    Server Ready

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,7 @@ dotenv.config()
 
 const localDBUrl = "mongodb://127.0.0.1/owner_taxi_db"
 // process.env.MONGODB_URL
-mongoose.connect(localDBUrl)
+mongoose.connect(process.env.MONGODB_URL)
 const db = mongoose.connection
 
 db.on('error' , console.error.bind(console , `ERROR CONNECTING TO ${localDBUrl}`))
